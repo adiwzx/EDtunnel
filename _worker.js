fccdf49a0d51f4a98ab40231e036856a123b90aa
Artuger <64976386+adiwzx@users.noreply.github.com>
2024-02-29 11:15:42 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '22288ebf-deb0-46c9-9524-f1634e14f87d';
 
-const proxyIPs = ['artuger.com', 'sp.rweek.top'];
+const proxyIPs = ['artuger.com'];
 
 // if you want to use ipv6 or single proxyIP, please add comment at this line and remove comment at the next line
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
